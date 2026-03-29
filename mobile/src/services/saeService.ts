a07d879700b78859dbc471ba9ea051f6a1c92a6f
emmanuel-kouassi <115140578+emmanuel-kouassi@users.noreply.github.com>
2026-03-29 18:29:06 +0200

    Update saeService.ts

--- mobile/src/services/saeService.ts
+++ mobile/src/services/saeService.ts
@@ -12,7 +12,7 @@ const BASE_URL = 'https://api-mobile-ux6f.onrender.com/api'; // Android emulator
 
 const api = axios.create({
   baseURL: `${BASE_URL}`,
-  timeout: 10000,
+  timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 });
 
